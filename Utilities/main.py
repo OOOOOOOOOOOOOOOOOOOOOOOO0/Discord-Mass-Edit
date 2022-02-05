fcd2e881bc56a7bdf97343076806ffc2d4063b1c
c0mpt0 <93231391+execution@users.noreply.github.com>
2022-02-05 16:58:20 +0000

    Add files via upload

---
+++ Utilities/main.py
@@ -0,0 +1,29 @@
+from colorama import Fore
+import requests, sys, time, os
+
+class Colours:
+    Green = Fore.GREEN
+    Red = Fore.RED
+    Reset = Fore.RESET
+
+class Utilities:
+
+    def __init__(self):
+        self.ASCII_URL = "https://artii.herokuapp.com"
+        pass
+
+    def type(self, text):
+        for char in text + "\n\n":
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(1. / 7)
+
+    def Logo(self):
+        ascii = requests.get(self.ASCII_URL+"/make?text=Discord Editor")
+        print(f"{Colours.Green}{ascii.text}{Colours.Reset}")
+
+    def Clear(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
