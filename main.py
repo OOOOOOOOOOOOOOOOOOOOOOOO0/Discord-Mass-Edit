d9eaae891284d0c2ffc350d1e85d520b01ff14e3
c0mpt0 <93231391+execution@users.noreply.github.com>
2022-02-07 23:30:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord, string, random, os, json, re
+import discord, string, random, json
 from discord.ext import commands
 from Utilities import Utilities, Colours
 
@@ -10,7 +10,7 @@ with open('config.json') as f:
 token = configData["Token"]
 prefix = configData["Prefix"]
 
-bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all(), self_bot=True)
 bot.remove_command("help")
 
 @bot.event
@@ -19,41 +19,31 @@ async def on_connect():
     print(f"{Colours.Green} [>] Connected to {bot.user} ({bot.user.id}){Colours.Reset}")
     Utilities.type(f"{Colours.Green} [>] Waiting to edit... {Colours.Reset}")
 
-@bot.event
-async def on_message(message):
-    
-    channels = []
+@bot.command()
+async def edit(ctx, limit: int=None):
     messages = 0
-    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message.content.lower())
-
-    if message.content == f"{prefix}edit":
-        channels.append(message.channel)
-        await message.delete()
-    else:
-        return
-    for ch in channels:
-        try:
-            async for msg in ch.history(limit=None):
-                if msg.author == bot.user:
-                    print(f"{Colours.Green} [>] Edited: {msg.content} {Colours.Reset}")
-                try:
-                    letters = string.ascii_letters + string.digits
-                    str1ng = (''.join(random.choice(letters) for i in range(15)))
-                    await msg.edit(content=str1ng)
+    images = 0
+    try:
+        async for message in ctx.message.channel.history(limit=None).filter(lambda m: m.author == bot.user):
+            try:
+                letters = string.ascii_letters + string.digits
+                str1ng = (''.join(random.choice(letters) for i in range(15)))
+                await message.edit(content=str1ng)
+                messages +=1
+                if message.attachments:
                     await message.channel.purge(check=lambda m: m.attachments != [])
-                    if urls in msg:
-                        await message.channel.purge(urls)
-                    messages += 1
-
-                except:
-                    continue
-        except:
-            Utilities.type(f"{Colours.Red} [>] Can't read message history! {Colours.Reset}")
-        print(f"{Colours.Green} [>] Successfully edited {messages} messages in the channel: {ch}! {Colours.Reset}")
+                    images += 1
+                print(f'{Colours.Green} [>] Edited: {str1ng} {Colours.Reset}')
+            except:
+                continue 
+        Utilities.type(f'{Colours.Green} [>] Successfully edited {messages} messages and deleted {images} attatchments! {Colours.Reset}')
         Utilities.Clear()
         Utilities.Logo()
         Utilities.type(f"{Colours.Green} [>] Waiting to edit... {Colours.Reset}")
+    except:
+        Utilities.type(f"{Colours.Red} [>] Can't read message history! {Colours.Reset}")
 
 
 if __name__ == '__main__':
     bot.run(token, bot=False)
+
